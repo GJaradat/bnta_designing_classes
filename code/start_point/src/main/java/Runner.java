cfd45ea32e82bee430417453c3e7ea63fc3471d6
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-06 17:47:43 +0000

    Completed Part 3.

--- code/start_point/src/main/java/Runner.java
+++ code/start_point/src/main/java/Runner.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -56,18 +58,27 @@ public class Runner {
         System.out.println("Running shopping trolley tasks...");
 
         // 1. Create a new shopping trolley
-//        ShoppingTrolley trolley = <INSTANTIATE A NEW SHOPPINGTROLLEY OBJECT HERE>;
+        ShoppingTrolley trolley = new ShoppingTrolley();
 
         // 2. Add an item to the trolley
-//        <CALL YOUR METHOD HERE>;
+        trolley.addItem("milk");
+        trolley.addItem("bread");
+        trolley.addItem("cheese");
 
         // 3. Count items in the trolley
-//        int itemCount = <CALL YOUR METHOD HERE>;
-//        System.out.println("The trolley contains " + itemCount + " items.");
+        System.out.println("The trolley contains " + trolley.countItems() + " items.");
 
         // 4. Check if an item is in the trolley
-//        boolean itemInTrolley = <CALL YOUR METHOD HERE>;
-//        System.out.println("Object is a square (true/false): " + itemInTrolley);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which item are you looking for?");
+        String importantItem = scanner.nextLine();
+
+        if (trolley.checkItem(importantItem)){
+            System.out.println(importantItem + " is in the trolley!");
+        }
+        else{
+            System.out.println(importantItem + " is NOT in the trolley!");
+        }
     }
 
 
