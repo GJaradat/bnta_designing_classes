810047eae8ca56c11172cc5f59ea941c0f46c991
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-06 17:05:48 +0000

    Completed Computer class.

---
+++ code/start_point/src/main/java/Computer.java
@@ -0,0 +1,36 @@
+public class Computer {
+
+    private int storage;
+    private String printerModel;
+
+
+    public Computer(int storage) {
+        this.storage = storage;
+        this.printerModel = null;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int moreStorage) {
+        this.storage += moreStorage;
+    }
+
+    public String getPrinterModel() {
+        return printerModel;
+    }
+
+    public void setPrinterModel(String printerModel) {
+        this.printerModel = printerModel;
+    }
+
+    public void printMessage(String msg){
+        if (this.printerModel != null){
+            System.out.println(msg);
+        }
+    }
+
+        
+
+}
